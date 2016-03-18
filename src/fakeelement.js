5a9ede020838e110be813f5cb6ef3a50bd87ca0f
Jared Updike <jupdike@thebrain.com>
2016-03-18 16:33:25 -0700

    Working with width and height and XML headers, toDataUri(), etc.

--- src/fakeelement.js
+++ src/fakeelement.js
@@ -1,30 +1,30 @@
 // Vex Flow
 // Mohit Muthanna <mohit@muthanna.com>
 //
-// Fake element type that can be passed to SVGContext constructor.
+// Fake element type that can be passed to SVGContext constructor to create
+// SVG strings, offline, without resorting to jsdom or xmldom
 //
 // Copyright Mohit Muthanna 2015
 // @author Jared Updike (2016)
 
 /** @constructor */
 Vex.Flow.FakeElement = (function() {
-  function FakeElement(tag) {
+  function FakeElement(width, height) {
     //
-    this.init(tag);
+    this.init(width, height);
   }
   
-  function addslashes(str) {
+  function addSlashes(str) {
     return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
   }
 
-  FakeElement.constantGuy = 42;
+  FakeElement.outsideWrapper = 'outside-wrapper';
   
   FakeElement.prototype = {
-    init: function(tag) {
-    // TODO deal with this
-      this.width = 200; //width;
-      this.height = 200; //height;
-      this.tag = tag;
+    init: function(width, height) {
+      this.width = width;
+      this.height = height;
+      this.tag = FakeElement.outsideWrapper;
       this.children = [];
       this.attributes = {};
     },
@@ -33,10 +33,10 @@ Vex.Flow.FakeElement = (function() {
       this.children.push(child);
     },
     
-    // maybe make a second one: docCreate ?
     create: function(tag) {
-      console.log("FakeElement.create TODO deal with width / height");
-      return new FakeElement(tag);
+      var el = new FakeElement(0, 0);
+      el.tag = tag;
+      return el;
     },
     
     setAttributeNS: function(nsDummy, k, v) {
@@ -47,23 +47,46 @@ Vex.Flow.FakeElement = (function() {
       this.attributes[k] = v;
     },
 
-    _internalRender: function() {
-      var ret = '<'+this.tag+' ';
+    toDataUri: function() {
+      var svgAsString = this.render();
+      var encoded = btoa(svgAsString);
+      var uri = 'data:image/svg+xml;base64,' + encoded;
+      return uri;
+    },
+    
+    render: function(moreAttribs) {
+      // fancy case of rendering out the container to a nice standalone string,
+      // equivalent to an SVG file that can be loaded into a browser as an .svg image file,
+      // or converted into a data URL (see toDataUri() method)
+      if (this.tag === FakeElement.outsideWrapper && this.children && this.children.length == 1 &&
+        this.children[0].tag === 'svg') {
+        var w = this.width;
+        var h = this.height;
+        var ret = '<?xml version="1.0" encoding="utf-8"?>\n';
+        ret += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+        var attribs = ' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"\n';
+        attribs += '     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\n';
+        attribs += '     width="'+w+'px" height="'+h+'px" viewBox="0 0 '+w+' '+h+'"\n';
+        attribs += '     enable-background="new 0 0 '+w+' '+h+'" xml:space="preserve"';
+        ret += this.children[0].render(attribs);
+        return ret;
+      }
+      
+      // normal case of rendering simple nested XML bits to string
+      moreAttribs = moreAttribs || '';
+      if (Object.keys(this.attributes).length > 0) {
+        moreAttribs = ' ';
+      }
+      var ret = '<' + this.tag + moreAttribs;
       // TODO style tag string according to whether children and/or attributes or not!
       for (var prop in this.attributes) {
-        ret += prop + '="' + addslashes(this.attributes[prop]) + '"' + ' '; // escape the string
+        ret += prop + '="' + addSlashes(this.attributes[prop]) + '"' + ' '; // escape the string
       }
       ret += '>';
       for (var i = 0; i < this.children.length; i++) {
-        ret += this.children[i]._internalRender() + '\n';
+        ret += this.children[i].render() + '\n';
       }
       ret += '</'+this.tag+'>';
-      return ret;
-    },
-    
-    render: function() {
-      var ret = '<? XML >\nTODO header\n';
-      ret += this._internalRender();
       return ret;
     },
   };
